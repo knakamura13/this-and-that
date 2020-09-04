003f7365d9617ad587de1c69ef913a89a9606502
Kyle Nakamura <kjnakamura@apu.edu>
2020-09-03 22:39:36 -0700

    Refactor all generated files, add app icon, configure info.plist

---
+++ this-and-thatTests/this_and_thatTests.swift
@@ -0,0 +1,17 @@
+//
+//  this_and_thatTests.swift
+//
+
+import XCTest
+@testable import this_and_that
+
+class this_and_thatTests: XCTestCase {
+    
+    // This method is called before the invocation of each test method in the class.
+    override func setUpWithError() throws {
+    }
+
+    // This method is called after the invocation of each test method in the class.
+    override func tearDownWithError() throws {
+    }
+}
